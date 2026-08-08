a9f37eb0b784f7c03dd72b3aaef065d33ab491d8
Omar Nasser <omarnasserjr@gmail.com>
2026-08-08 11:05:36 +0300

    Add Secrets event handlers, ManagedSecret DeleteFunc, and unexport internal methods

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -4,33 +4,89 @@ import (
 	"fmt"
 	"time"
 
+	apiv1alpha1 "github.com/naseyro/ssc/pkg/apis/secretrotator/v1alpha1"
 	"github.com/naseyro/ssc/pkg/clientset/secretrotator/v1alpha1"
+	"github.com/naseyro/ssc/pkg/informers"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
 )
 
 type Controller struct {
-	msClientSet v1alpha1.SecretRotatorV1Alpha1Interface
-	workqueue   workqueue.TypedRateLimitingInterface[string]
-	msInformer  cache.SharedIndexInformer
+	msClientSet    v1alpha1.SecretRotatorV1Alpha1Interface
+	workqueue      workqueue.TypedRateLimitingInterface[string]
+	msInformer     cache.SharedIndexInformer
+	secretsInfomer cache.SharedIndexInformer
 }
 
-func NewController(c v1alpha1.SecretRotatorV1Alpha1Interface) *Controller {
+func NewController(sc v1alpha1.SecretRotatorV1Alpha1Interface, kc kubernetes.Interface) *Controller {
 	workqueue := workqueue.NewTypedRateLimitingQueue(workqueue.DefaultTypedControllerRateLimiter[string]())
-	managedSecretsInformer := NewSharedIndexInformer(c)
+	managedSecretsInformer := informers.NewSharedIndexInformer(sc)
+	secretsInformer := informers.NewSecretsSharedInformer(kc)
 	ctr := &Controller{
-		msClientSet: c,
-		workqueue:   workqueue,
-		msInformer:  managedSecretsInformer,
+		msClientSet:    sc,
+		workqueue:      workqueue,
+		msInformer:     managedSecretsInformer,
+		secretsInfomer: secretsInformer,
 	}
 	managedSecretsInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc:    ctr.AddFunc,
 		UpdateFunc: ctr.UpdateFunc,
-		// Implement DeleteFunc
+		DeleteFunc: ctr.DeleteFunc,
+	})
+
+	secretsInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    ctr.secretsAddFunc,
+		UpdateFunc: ctr.secretsUpdateFunc,
+		DeleteFunc: ctr.secretsDeleteFunc,
 	})
 	return ctr
+}
+
+func (c *Controller) secretsAddFunc(obj interface{}) {
+	secret, ok := obj.(*corev1.Secret)
+	if !ok {
+		runtime.HandleError(fmt.Errorf("error converting objecto to Secret"))
+		return
+	}
+	c.handleSecret(secret)
+}
+
+func (c *Controller) secretsUpdateFunc(oldObj, newObj interface{}) {
+	oldMeta, oldOk := oldObj.(metav1.Object)
+	newMeta, newOk := newObj.(metav1.Object)
+	if oldOk && newOk && oldMeta.GetResourceVersion() == newMeta.GetResourceVersion() {
+		return
+	}
+
+	secret, ok := newObj.(*corev1.Secret)
+	if !ok {
+		runtime.HandleError(fmt.Errorf("error converting objecto to Secret"))
+		return
+	}
+	c.handleSecret(secret)
+}
+
+func (c *Controller) secretsDeleteFunc(obj interface{}) {
+	secret, ok := obj.(*corev1.Secret)
+	if !ok {
+		tombstone, ok := obj.(cache.DeletedFinalStateUnknown)
+		if !ok {
+			runtime.HandleError(fmt.Errorf("error decoding tombstone object, invalid type"))
+			return
+		}
+		secret, ok = tombstone.Obj.(*corev1.Secret)
+		if !ok {
+			runtime.HandleError(fmt.Errorf("error decoding secret object, invalid type"))
+			return
+		}
+	}
+
+	c.handleSecret(secret)
 }
 
 func (c *Controller) AddFunc(obj interface{}) {
@@ -43,13 +99,42 @@ func (c *Controller) AddFunc(obj interface{}) {
 }
 
 func (c *Controller) UpdateFunc(oldObj interface{}, newObj interface{}) {
-	// Read Optimization from Gemini
+	oldMeta, oldOk := oldObj.(metav1.Object)
+	newMeta, newOk := newObj.(metav1.Object)
+
+	if oldOk && newOk && oldMeta.GetResourceVersion() == newMeta.GetResourceVersion() {
+		return
+	}
+
 	key, err := cache.MetaNamespaceKeyFunc(newObj)
 	if err != nil {
 		runtime.HandleError(err)
 		return
 	}
 	c.workqueue.Add(key)
+}
+
+func (c *Controller) DeleteFunc(obj interface{}) {
+	ms, ok := obj.(*apiv1alpha1.ManagedSecret)
+
+	if !ok {
+		tombstone, ok := obj.(cache.DeletedFinalStateUnknown)
+		if !ok {
+			runtime.HandleError(fmt.Errorf("couldn't get object from tombstone"))
+			return
+		}
+
+		ms, ok = tombstone.Obj.(*apiv1alpha1.ManagedSecret)
+		if !ok {
+			runtime.HandleError(fmt.Errorf("tombstone contained object that is not a ManagedSecret"))
+			return
+		}
+	}
+
+	key, err := cache.MetaNamespaceKeyFunc(ms)
+	if err == nil {
+		c.workqueue.Add(key)
+	}
 }
 
 func (c *Controller) Run() error {
@@ -60,19 +145,19 @@ func (c *Controller) Run() error {
 	if !cache.WaitForCacheSync(stopCh, c.msInformer.HasSynced) {
 		return fmt.Errorf("error syncing the ManagedSecrets Informer Cache")
 	}
-	wait.Until(c.Worker, time.Second, stopCh)
+	wait.Until(c.worker, time.Second, stopCh)
 	<-stopCh
 
 	return nil
 }
 
-func (c *Controller) Worker() {
-	for c.ProccessNextItem() {
+func (c *Controller) worker() {
+	for c.proccessNextItem() {
 
 	}
 }
 
-func (c *Controller) ProccessNextItem() bool {
+func (c *Controller) proccessNextItem() bool {
 	key, shutdown := c.workqueue.Get()
 	defer c.workqueue.Done(key)
 
@@ -80,7 +165,7 @@ func (c *Controller) ProccessNextItem() bool {
 		return false
 	}
 
-	if err := Reconcile(key); err != nil {
+	if err := reconcile(key); err != nil {
 		runtime.HandleError(err)
 		c.workqueue.AddRateLimited(key)
 		return true
@@ -89,6 +174,28 @@ func (c *Controller) ProccessNextItem() bool {
 	return true
 }
 
-func Reconcile(key string) error {
+func reconcile(key string) error {
 	return nil
+}
+
+func (c *Controller) handleSecret(secret *corev1.Secret) {
+	matchedObjects, err := c.msInformer.GetIndexer().ByIndex("secretName", secret.Name)
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+
+	for _, matchedObj := range matchedObjects {
+		ms, ok := matchedObj.(*apiv1alpha1.ManagedSecret)
+		if !ok {
+			continue
+		}
+
+		if ms.Namespace == secret.Namespace {
+			key, err := cache.MetaNamespaceKeyFunc(ms)
+			if err == nil {
+				c.workqueue.Add(key)
+			}
+		}
+	}
 }
