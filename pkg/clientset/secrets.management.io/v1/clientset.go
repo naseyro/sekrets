8225386714ae4e43a651f7f8ea96c11d7405acce
Omar Nasser <omarnasserjr@gmail.com>
2026-08-08 18:26:58 +0300

    refactor: remove discovery interface

--- pkg/clientset/secrets.management.io/v1/clientset.go
+++ pkg/clientset/secrets.management.io/v1/clientset.go
@@ -2,26 +2,11 @@ package v1
 
 import (
 	v1 "github.com/naseyro/ssc/pkg/apis/secrets.management.io/v1"
-	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 )
 
-// TODO: We can create inner interface to include multiple API Versions
-// Instead of SecretRotatorV1Alpha1Interface it should be
-// type SecretRotatorInterface interface
-// and it has multiple functions about API Versions
-/*
-type SecretRotatorInterface {
-	V1Alpha1()
-	V1Beta1()
-	V1()
-}
-And inside every version we should implement the ManagedSecret type based on their types.go
-*/
-
 type Interface interface {
-	Discovery() discovery.DiscoveryInterface
 	SecretsManagementV1() SecretsManagementV1Interface
 }
 
