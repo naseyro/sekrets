79ec713d2fe89d42da9cc1c0021aa0c73212ef7f
Omar Nasser <omarnasserjr@gmail.com>
2026-08-14 09:05:39 +0300

    feat: add graceful shutdown with signal handling and use klog.Fatalf for fatal errors

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	SecretsManagementClientset "github.com/naseyro/ssc/pkg/clientset/secrets.management.io/v1"
 	"github.com/naseyro/ssc/pkg/controller"
@@ -14,30 +18,40 @@ import (
 )
 
 func main() {
+	klog.InitFlags(nil)
+
 	kubeconfig := flag.String("kubeconfig", "/Volumes/external/Users/onasser/.kube/config", "kubeconfig file")
+	flag.Parse()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
 	restConfig, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
-		klog.Errorf("error building the config file: %v", err)
+		klog.Fatalf("error building the config file: %v", err)
 	}
 
 	smClientset, err := SecretsManagementClientset.NewForConfig(restConfig)
 	if err != nil {
-		klog.Errorf("error creating ManagedSecret clientset: %v", err)
+		klog.Fatalf("error creating ManagedSecret clientset: %v", err)
 	}
 
 	kcs, err := kubernetes.NewForConfig(restConfig)
 	if err != nil {
-		klog.Errorf("error building the Kubernetes Secrets clientset: %v", err)
+		klog.Fatalf("error building the Kubernetes Secrets clientset: %v", err)
 	}
 	dynamicClient, err := dynamic.NewForConfig(restConfig)
 	if err != nil {
-		klog.Errorf("error building the Dynamic Kubernetes clientset: %v", err)
+		klog.Fatalf("error building the Dynamic Kubernetes clientset: %v", err)
 	}
 
 	discoveryClient, err := discovery.NewDiscoveryClientForConfig(restConfig)
 	if err != nil {
 		log.Fatalf("Error creating discovery client: %v", err)
 	}
-
 	msController := controller.NewController(smClientset, kcs, dynamicClient, discoveryClient)
+
+	if err := msController.Run(5, ctx.Done()); err != nil {
+		klog.Fatalf("Error running controller: %v", err)
+	}
 }
